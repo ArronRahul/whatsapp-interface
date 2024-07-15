0db972ad1a3fd60dbf1cbf9d587ced50dbc53f1d
RAHUL A P <rahul.ap@aot-technologies.com>
2024-07-15 13:45:53 +0530

    last-commit

--- src/component/PersonalChats.jsx
+++ src/component/PersonalChats.jsx
@@ -4,7 +4,29 @@ import './PersonalChats.css'
 function PersonalChats() {
   return (
     <div className='PersonalChat'>
+      <div className='chat-navbar'>
+        <div className='navbar-profile'>
+            <img src=''></img>
+        </div>
+        <div className='navbar-name'>
+          <p>Arun</p>
+        </div>
+        <div className='navbar-end'>
+          <div className='navbar-end1'>
+          <img src='src/assets/icons8-video-call-24.png'></img>
+          </div>
+          <img src='src/assets/icons8-search-24.png'></img>
+          <img src='src/assets/icons8-menu-vertical-24.png'></img>
+        </div>
+      </div>
+      <div className='chat-body'>
         
+      </div>
+      <div className='chat-botbar'>
+        
+
+
+      </div>
     </div>
   )
 }
